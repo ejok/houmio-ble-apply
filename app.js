6e746583e9b966a65461bc80f64a747dbdd2d9ab
Eeli Jokinen <eeli.jokinen@gmail.com>
2014-10-13 23:31:17 +0300

    WIP: add support for applying a specific scene based on standard output of a child process. NOTE: heavily work in progress.

--- app.js
+++ app.js
@@ -1,2 +1,46 @@
-console.log('Houm.io BLE app goes here');
+var spawn = require('child_process').spawn;
+var https = require('https');
+var Bacon = require('baconjs');
+var _ = require('lodash');
 
+var HOUMIO_SITEKEY = process.env.HOUMIO_SITEKEY;
+var HOUMIO_SCENEID = process.env.HOUMIO_SCENEID;
+
+var beaconScanCmd = spawn('./ibeacon_scan_debug', ['-b']);
+
+function splitLines(bufferedData) {
+	return Bacon.fromArray(bufferedData.toString('utf-8').split('\n'));
+}
+
+function isNotEmpty(line) {
+	return !_.isEmpty(line);
+}
+
+var scannedBeaconsStream = Bacon.fromEventTarget(beaconScanCmd.stdout, 'data')
+	.flatMap(splitLines)
+	.filter(isNotEmpty)
+	.skipDuplicates();
+
+scannedBeaconsStream.take(1).onValue(applyScene);
+
+function applyScene() {
+
+	console.log('Scanned known BLE advertisement message. Applying scene...');
+
+	var options = {
+		method: 'PUT',
+		hostname: 'houm.herokuapp.com',
+		path: '/api/site/' + HOUMIO_SITEKEY + '/scene/apply',
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	};
+
+	var req = https.request(options, function(res) {
+		console.log('STATUS: ' + res.statusCode);
+	});
+	req.write(JSON.stringify({
+		_id: HOUMIO_SCENEID
+	}));
+	req.end();
+}
